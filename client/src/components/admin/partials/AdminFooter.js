decd6a61ea66abbd5bea19364f634a192a439d63
lively06 <91404849+lively06@users.noreply.github.com>
2023-04-17 13:38:54 +0530

    HANDMADE PRODUCTS E- COMMERCE WEBSITE
    code for e commerce website

---
+++ client/src/components/admin/partials/AdminFooter.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import moment from "moment";
+
+const AdminFooter = (props) => {
+  return (
+    <Fragment>
+      
+    </Fragment>
+  );
+};
+
+export default AdminFooter;
